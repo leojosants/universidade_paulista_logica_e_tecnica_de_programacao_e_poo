918952e22fef2efb41029a9d4b62ec6696cc6931
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-07 19:11:17 -0300

    Pergunta 1

---
+++ Disciplina-POO_1/Unidade_2/Atividade_Teleaula/Pergunta1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Pergunta1
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int numero = 7;
+
+            bool numeroPar = numero % 2 == 0;
+            bool menorQueCinco = numero < 5;
+
+            if (numeroPar || menorQueCinco)
+            {
+                Console.WriteLine("O número é par e émenor que cinco!");
+            }
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
