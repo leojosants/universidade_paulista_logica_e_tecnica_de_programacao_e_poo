831ef9f5fc56998e57e673c8058a033fd751aaa6
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-07 15:26:17 -0300

    Laço de repetição - WHILE

---
+++ Disciplina-POO_1/Unidade_2/LacoDeRepeticao/LacoDeRepeticaoWHILE/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace LacoDeRepeticaoWHILE {
+    
+    class Program {
+
+        static void Main(string[]args) {
+
+            Console.WriteLine("");
+
+            int i = 1;
+
+            while (i < 10) {
+                Console.WriteLine("Iteração: {0}", i);
+                i++;
+            }
+
+            Console.WriteLine("\nPrograma finalizado!\n");
+
+        }
+
+    }
+
+}
