e55ae4209fb5c0cecdb79deb37f0ef79768298ec
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-07 15:11:53 -0300

    Laço de repetição -FOR

---
+++ Disciplina-POO_1/Unidade_2/LacoDeRepeticao/LacoDeRepeticaoFOR/Program.cs
@@ -0,0 +1,42 @@
+﻿
+namespace LacoDeRepeticaoFOR {
+    
+    class Program {
+
+        static void Main(string[]args) {
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine("Iteração: {0}", i);
+            }
+
+            Console.WriteLine("----------/----------/----------");
+
+            for (int i = 0; i < 5; i++) {
+
+                if (i == 3) {
+                    break;
+                }
+
+                Console.WriteLine("Iteração: {0}", i);
+            }
+
+            Console.WriteLine("----------/----------/----------");
+
+            for (int i = 0; i < 5; i++) {
+
+                if (i == 3) {
+                    continue;
+                }
+
+                Console.WriteLine("Iteração: {0}", i);
+            }
+
+            Console.WriteLine("\nPrograma finalizado!\n");
+
+        }
+
+    }
+
+}
