d5c2bd75bcfc7b612d70b9eede9d6089194f6f01
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-07 16:49:23 -0300

    Pergunta 2

---
+++ Disciplina-POO_1/Unidade_2/Questionario/Pergunta2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Pergunta2
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int a = 10;
+            int b = 6;
+            int c = 2;
+            int d = 3;
+
+            Console.WriteLine(a + b / c - d);
+
+        }
+
+    }
+
+}
