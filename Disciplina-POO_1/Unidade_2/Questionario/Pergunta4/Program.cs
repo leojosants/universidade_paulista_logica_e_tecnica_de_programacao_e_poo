7145293cfbdcc6fc778f37940842ca120837c8f3
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-07 17:00:04 -0300

    Pergunta 4

---
+++ Disciplina-POO_1/Unidade_2/Questionario/Pergunta4/Program.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Pergunta4
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            for (int i = 10; i != 0; i -= 2)
+            {
+                Console.WriteLine("i = {0}", i);
+            }
+        }
+
+    }
+
+}
