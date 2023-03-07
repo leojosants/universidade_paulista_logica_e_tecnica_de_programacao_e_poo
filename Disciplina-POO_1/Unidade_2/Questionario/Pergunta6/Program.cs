62a32b35d5bc9049d0a12563acb898eb2cb6fde9
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-07 17:10:51 -0300

    Pergunta 6

---
+++ Disciplina-POO_1/Unidade_2/Questionario/Pergunta6/Program.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Pergunta6
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string s = "Manipulação de strings".Substring(7, 4);
+
+            Console.WriteLine(s);
+
+        }
+
+    }
+
+}
