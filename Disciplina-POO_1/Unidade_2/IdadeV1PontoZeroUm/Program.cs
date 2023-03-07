112148908ca38b86db4ba351559036104c3fb9f7
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-07 11:43:37 -0300

    Programa Idade versão 1.01

---
+++ Disciplina-POO_1/Unidade_2/IdadeV1PontoZeroUm/Program.cs
@@ -0,0 +1,35 @@
+﻿
+namespace IdadeV1PontoZeroUm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade, idadeAnoQueVem;
+            
+            idade = 15;
+            idadeAnoQueVem = idade + 1;
+
+            Console.WriteLine();
+            Console.WriteLine("Programa Idade versão 1.01");
+            Console.WriteLine();
+
+            Console.WriteLine("A idade atual é {0} anos e ano que vem será {1} anos.", idade, idadeAnoQueVem);
+            Console.WriteLine();
+
+        }
+    }
+}
+
+/*
+
+     Operadores matemáticos em C#
+
+        Símbolo     Operador matemático
+        +           Adição
+        -           Subtração
+        *           Multiplicação
+        /           Divisão
+        %           Resto da divisão
+
+*/
