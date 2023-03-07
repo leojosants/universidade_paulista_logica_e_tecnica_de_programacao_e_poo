9dafec408aaa96b50cef4cc8e5edee8d1952bf09
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-07 15:31:30 -0300

    Laço de repetição - DO WHILE

---
+++ Disciplina-POO_1/Unidade_2/LacoDeRepeticao/LacoDeRepeticaoDOWHILE/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace LacoDeRepeticaoDOWHILE {
+    
+    class Program {
+
+        static void Main(string[]args) {
+
+            Console.WriteLine("");
+
+            int i = 1;
+
+            do {
+                Console.WriteLine("Iteração: {0}", i);
+                i++;
+            } while (i < 10);
+
+            Console.WriteLine("\nPrograma finalizado!\n");
+
+        }
+
+    }
+
+}
