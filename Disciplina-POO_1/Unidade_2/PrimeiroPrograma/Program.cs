9cff65bca17688fe077b40e13a27a1ea453c8c27
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-07 10:32:13 -0300

    Meu primeiro programa em C#.

---
+++ Disciplina-POO_1/Unidade_2/PrimeiroPrograma/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PrimeiroPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // escrevendo no console
+            Console.WriteLine("Olá, Mundo em C#!");
+            Console.ReadLine();
+        }
+    }
+}
