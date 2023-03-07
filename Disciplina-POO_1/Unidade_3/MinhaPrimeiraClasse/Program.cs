76b4075bec70639ebd41b18e9f5214a41aebaf7c
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-07 19:50:32 -0300

    Declaração de atributos e intanciaento de objeto.

---
+++ Disciplina-POO_1/Unidade_3/MinhaPrimeiraClasse/Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace MinhaPrimeiraClasse
+{
+
+    class Conta
+    {
+        public double saldo;
+        public double limite;
+        public int conta;
+    }
+
+    // class categoria
+
+    // class TipoPagamento
+
+    // class Caixa
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta referencia;
+
+            referencia = new Conta();
+            referencia.saldo = 1000.0;
+            referencia.limite = 300;
+            referencia.conta = 123;
+
+        }
+
+    }
+
+}
