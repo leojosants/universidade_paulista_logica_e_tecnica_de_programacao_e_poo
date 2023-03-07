ac405da08f559cc2390db6275f19e72a12ba7052
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-07 16:54:00 -0300

    Pergunta 3

---
+++ Disciplina-POO_1/Unidade_2/Questionario/Pergunta3/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Pergunta2
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            bool primeiro = 7 > 5 && 7 % 5 == 1;
+            bool segumdo = 4 * 3 != 10 && !(3 > 3);
+
+            Console.WriteLine(primeiro);
+            Console.WriteLine(segumdo);
+
+        }
+
+    }
+
+}
