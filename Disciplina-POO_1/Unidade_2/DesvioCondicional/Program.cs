691260c42f27ada2fe6fa9dec5d88301becaad53
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-07 13:37:47 -0300

    Desvio condicional - Teste condicional 1

---
+++ Disciplina-POO_1/Unidade_2/DesvioCondicional/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DesvioCondicional
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            double numero = 0;
+
+            Console.Write("\nDigite um número: ");
+
+            numero = double.Parse(Console.ReadLine());
+
+            if(numero > 20) {
+                Console.WriteLine("A metade de {0} é: {1}", numero, (numero / 2));
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
