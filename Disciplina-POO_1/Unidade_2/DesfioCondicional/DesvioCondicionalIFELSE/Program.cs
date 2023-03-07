792419f301e597d077154d59d25602413bab7835
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-07 14:04:15 -0300

    Desvio condicional - IF ELSE

---
+++ Disciplina-POO_1/Unidade_2/DesfioCondicional/DesvioCondicionalIFELSE/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace DesvioCondicionalIFELSE
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            double numero = 0;
+
+            Console.Write("\nDigite um número: ");
+
+            numero = Convert.ToDouble(Console.ReadLine());
+
+            if (numero % 2 == 0) {
+                Console.WriteLine("O número {0} é: PAR", numero);
+            }
+            else {
+                Console.WriteLine("O número {0} é: ÍMPAR", numero);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
