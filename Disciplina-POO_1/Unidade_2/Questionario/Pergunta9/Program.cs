e9318f8c1fefa718f5bc330b40f89197ad6cba3f
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-07 17:21:11 -0300

    Pergunta 9

---
+++ Disciplina-POO_1/Unidade_2/Questionario/Pergunta9/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Pergunta9
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            // string s = Console.ReadLine();
+
+            // long i = Convert.ToInt64(Console.ReadLine());
+
+            // int i = Console.ReadLine(); // ESTA LINHA APRESENTA ERRO NÃO COMPILA
+
+            // float f = Single.Parse(Console.ReadLine());
+
+            double d = Convert.ToDouble(Console.ReadLine());
+
+        }
+
+    }
+
+}
