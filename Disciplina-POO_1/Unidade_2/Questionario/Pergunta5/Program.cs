2ed8f2862006edca2603860b205c5deedc0ba99c
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-07 17:07:12 -0300

    Pergunta 5

---
+++ Disciplina-POO_1/Unidade_2/Questionario/Pergunta5/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Pergunta5
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int i;
+
+            for (i = 0; i < 10; i++)
+            {
+
+                if (i > 0) {
+                    continue;
+                }
+
+            }
+
+            Console.WriteLine("i = {0}", i);
+
+        }
+
+    }
+
+}
