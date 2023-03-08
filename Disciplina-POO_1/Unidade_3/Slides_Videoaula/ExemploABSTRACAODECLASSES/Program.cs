7425e6d0e0880e4328d6a072c9a92d7b124e9946
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-08 19:00:50 -0300

    Atualização de arquivos

---
+++ Disciplina-POO_1/Unidade_3/Slides_Videoaula/ExemploABSTRACAODECLASSES/Program.cs
@@ -0,0 +1,16 @@
+﻿// Início namespace ExemploABSTRACAODECLASSES
+namespace ExemploABSTRACAODECLASSES
+{
+
+    // Início class Program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n -- Fim do Programa -- \n");
+        }
+    }
+    // Fim class Program
+
+}
+// Fim namespace ExemploABSTRACAODECLASSES
