e6253442305ed51f324e3156e3f484fef7cc5f41
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-07 21:04:38 -0300

    Classe Gente-Passagem de endereço por referencia.

--- Disciplina-POO_1/Unidade_3/ExemploClasseGente/Program.cs
+++ Disciplina-POO_1/Unidade_3/ExemploClasseGente/Program.cs
@@ -1,9 +1,30 @@
 ﻿
-namespace  {
+namespace ExemploClasseGente
+{
+    class Gente
+    {
+        // Atributo
+        public String nome;
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+
+            Gente pessoa1 = new Gente();
+            Gente pessoa2 = new Gente();
+
+            pessoa1.nome = "Paulo";
+            pessoa1 = pessoa2;
+
+            pessoa2.nome = "Maria";
+
+            Console.WriteLine("\nPessoa1 nome: {0}", pessoa1.nome);
+            Console.WriteLine("\n -- Fim do Programa -- \n");
+
         }
+
     }
+
 }
