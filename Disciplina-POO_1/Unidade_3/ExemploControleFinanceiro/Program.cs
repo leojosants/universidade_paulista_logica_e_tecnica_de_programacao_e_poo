ffda4511f889498a228989576d43b07788aec1e9
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-07 21:49:18 -0300

    Criado NAMESPACE e adicionado atributos.

---
+++ Disciplina-POO_1/Unidade_3/ExemploControleFinanceiro/Program.cs
@@ -0,0 +1,40 @@
+﻿
+namespace ExemploControleFinanceiro
+{
+
+    class Conta
+    {
+        // Atributos
+        public String nome;
+        public double valor;
+        public String dataDeVencimento;
+        public String dataDePagamento;
+    }
+
+    class Categoria
+    {
+        // Atributos
+        public String nome;
+        public String tipo;
+    }
+
+    class TipoDePagamento
+    {
+        public String nome;
+    }
+
+    class Caixa
+    {
+        public String nome;
+        public double saldo;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+}
