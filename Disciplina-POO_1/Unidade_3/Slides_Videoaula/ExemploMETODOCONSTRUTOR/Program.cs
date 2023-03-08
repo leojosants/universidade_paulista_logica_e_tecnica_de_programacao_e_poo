8452db70310d30b59741f74793a85dce2352929f
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-08 19:54:02 -0300

    Método construtor.

---
+++ Disciplina-POO_1/Unidade_3/Slides_Videoaula/ExemploMETODOCONSTRUTOR/Program.cs
@@ -0,0 +1,39 @@
+﻿// Início namespace ExemploMETODOCONSTRUTOR
+namespace ExemploMETODOCONSTRUTOR
+{
+
+    // Início class Data
+    class Data
+    {
+        // Atributos
+        public int dia;
+        public int mes;
+        public int ano;
+
+        // Construtor
+        public Data(int dia, int mes, int ano)
+        {
+            dia = dia;
+            mes = mes;
+            ano = ano;
+        }
+    }
+    // Fim class Data
+
+    // Início class Program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data data1 = new Data(25, 12, 2015);
+
+            Console.WriteLine("\n Pressione ENTER para finalizar o programa!");
+            Console.ReadKey();
+            Console.WriteLine("\n -- Fim do Programa -- \n");
+
+        }
+    }
+    // Fim class Program
+
+}
+// Fim namespace ExemploMETODOCONSTRUTOR
