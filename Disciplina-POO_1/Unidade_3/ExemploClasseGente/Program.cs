be9d0cfac747d7aa26934b77ae629b1caf85193b
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-07 22:40:38 -0300

    Sobrecarga do Método Construtor

--- Disciplina-POO_1/Unidade_3/ExemploClasseGente/Program.cs
+++ Disciplina-POO_1/Unidade_3/ExemploClasseGente/Program.cs
@@ -5,23 +5,56 @@ namespace ExemploClasseGente
     {
         // Atributo
         public String nome;
+        public int idade;
+
+        // Método construtor
+        public Gente()
+        {
+            nome = "José";
+            idade = 20;
+        }
+
+        // Método construtor - sobrecarga
+        public Gente(String valor)
+        {
+            nome = valor;
+            idade = 20;
+        }
+
+        // Método construtor - sobrecarga
+        public Gente(int valor)
+        {
+            nome = "José";
+            idade = valor;
+        }
+
+        // Método construtor - sobrecarga
+        public Gente(String valorNome, int valorIdade)
+        {
+            nome = valorNome;
+            idade = valorIdade;
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
+            Gente pessoa1 = new Gente("Maria");
 
-            Gente pessoa1 = new Gente();
-            Gente pessoa2 = new Gente();
-
-            pessoa1.nome = "Paulo";
-            pessoa1 = pessoa2;
-
-            pessoa2.nome = "Maria";
-
-            Console.WriteLine("\nPessoa1 nome: {0}", pessoa1.nome);
+            Console.WriteLine("\nPessoa1 Nome: {0} | Idade: {1}", pessoa1.nome, pessoa1.idade);
             Console.WriteLine("\n -- Fim do Programa -- \n");
+
+            /*
+                Gente pessoa1 = new Gente();
+                Gente pessoa2 = new Gente();
+
+                pessoa1.nome = "Paulo";
+                pessoa1 = pessoa2;
+
+                pessoa2.nome = "Maria";
+
+            */
 
         }
 
