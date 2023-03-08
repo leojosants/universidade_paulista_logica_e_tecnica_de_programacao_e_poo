e238b600352a310771aec76b95c7c264dc28a37d
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-08 19:46:54 -0300

    Exemplo de método personalizado.

---
+++ Disciplina-POO_1/Unidade_3/Slides_Videoaula/ExemploMETODOS/Program.cs
@@ -0,0 +1,52 @@
+﻿// Início namespace ExemploMETODOS
+namespace ExemploMETODOS
+{
+
+    // Início class Retangulo
+    class Retangulo
+    {
+        // Atributos
+        public double largura = 0;
+        public double altura = 0;
+
+        // Métodos
+        public double CalculaArea()
+        {
+            return largura * altura;
+        }
+
+        public void AumentarLargura(double largura)
+        {
+            this.largura += largura;
+        }
+        public void AumentarAltura(double altura)
+        {
+            this.altura += altura;
+        }
+    }
+    // Fim class Retangulo 
+
+    // Início class Program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo1 = new Retangulo();
+            retangulo1.AumentarAltura(10);
+            retangulo1.AumentarLargura(15);
+
+            double area = retangulo1.CalculaArea();
+
+            Console.WriteLine();
+            Console.WriteLine("A área do retângulo de altura {0} e largura {1} vale {2}!", retangulo1.altura, retangulo1.largura, area);
+
+            Console.WriteLine("\n Pressione ENTER para finalizar o programa!");
+            Console.ReadKey();
+            Console.WriteLine("\n -- Fim do Programa -- \n");
+
+        }
+    }
+    // Fim class Program
+
+}
+// Fim namespace ExemploMETODOS
