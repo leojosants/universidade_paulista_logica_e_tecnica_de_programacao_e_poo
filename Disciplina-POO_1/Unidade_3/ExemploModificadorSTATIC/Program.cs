ffe76244ee6f754bef8a999aabf369a5c790b3ac
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-08 13:00:54 -0300

    Modificador STATIC

---
+++ Disciplina-POO_1/Unidade_3/ExemploModificadorSTATIC/Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace ExemploModificadorSTATIC
+{
+    public class Exemplo
+    {
+        // Atributos
+        private static int seq = 0;
+        public int nro;
+
+        // Construtor
+        public Exemplo()
+        {
+            nro = ++seq;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exemplo e1 = new Exemplo();
+            Console.WriteLine(e1.nro);
+
+            Exemplo e2 = new Exemplo();
+            Console.WriteLine(e2.nro);
+
+            Exemplo e3 = new Exemplo();
+            Console.WriteLine(e3.nro);
+
+            Console.WriteLine("\n -- Fim do Programa -- \n");
+        }
+    }
+}
