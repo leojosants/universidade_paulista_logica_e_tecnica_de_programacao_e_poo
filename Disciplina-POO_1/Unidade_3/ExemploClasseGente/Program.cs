84ff1646c6164e07d2b21de69092ff8bccf2940b
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-07 23:15:14 -0300

    Uso da palavra THIS

--- Disciplina-POO_1/Unidade_3/ExemploClasseGente/Program.cs
+++ Disciplina-POO_1/Unidade_3/ExemploClasseGente/Program.cs
@@ -9,31 +9,34 @@ namespace ExemploClasseGente
 
         // Método construtor
         public Gente()
+            : this("Jose", 20); // nome = "José"; idade = 20;
         {
-            nome = "José";
-            idade = 20;
         }
 
         // Método construtor - sobrecarga
-        public Gente(String valor)
+        public Gente(String nome)
+            : this(nome, 20); // nome = valor; idade = 20;
         {
-            nome = valor;
-            idade = 20;
         }
 
         // Método construtor - sobrecarga
         public Gente(int valor)
+            : this("José",valor); //nome = "José"; idade = valor;
         {
-            nome = "José";
-            idade = valor;
+        }
+
+          public Gente(String nome, int idade)
+        {
+            this.nome = nome;
+            this.idade = idade;
         }
 
         // Método construtor - sobrecarga
-        public Gente(String valorNome, int valorIdade)
+        /*public Gente(String valorNome, int valorIdade)
         {
             nome = valorNome;
             idade = valorIdade;
-        }
+        }*/
     }
 
     class Program
@@ -41,6 +44,7 @@ namespace ExemploClasseGente
         static void Main(string[] args)
         {
             Gente pessoa1 = new Gente("Maria");
+            Gente pessoa2 = new Gente("Paulo", 23);
 
             Console.WriteLine("\nPessoa1 Nome: {0} | Idade: {1}", pessoa1.nome, pessoa1.idade);
             Console.WriteLine("\n -- Fim do Programa -- \n");
@@ -54,6 +58,7 @@ namespace ExemploClasseGente
 
                 pessoa2.nome = "Maria";
 
+                Console.WriteLine("\n -- Fim do Programa -- \n");
             */
 
         }
