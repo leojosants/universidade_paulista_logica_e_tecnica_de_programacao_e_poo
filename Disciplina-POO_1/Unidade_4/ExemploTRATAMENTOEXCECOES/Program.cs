3bc2a87c825a024ec332759421d1c9c8c55e0c6a
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-09 16:55:18 -0300

    Exemplo de tratamento de exceções - Tratando erro.

--- Disciplina-POO_1/Unidade_4/ExemploTRATAMENTOEXCECOES/Program.cs
+++ Disciplina-POO_1/Unidade_4/ExemploTRATAMENTOEXCECOES/Program.cs
@@ -2,17 +2,65 @@
 namespace ExemploTRATAMENTOEXCECOES
 {
 
+    // Início class Funcionario
+    class Funcionario
+    {
+        // Método
+        public void AumentaSalario(double aumento)
+        {
+            if (aumento < 0)
+            {
+                ArgumentException erro = new ArgumentException();
+                throw erro;
+            }
+        }
+    }
+    // Fim class Funcionario
+
     // Início class Program
     class Program
     {
         // Início Main
         static void Main(string[] args)
         {
-            String v = "ABC";
-            double num = Convert.ToDouble(v); // Converte um string em número
+            /* TESTE 1
 
-            Console.WriteLine();
-            Console.WriteLine(num);
+                // String v = "Três";
+                String v2 = "3";
+
+                try
+                {
+                    Console.WriteLine();
+
+                    // double num = Convert.ToDouble(v); // Converte um string em número
+                    // Console.WriteLine(num);
+
+                    double num2 = Convert.ToDouble(v2); // Converte um string em número
+                    Console.WriteLine(num2);
+                }
+                catch (System.FormatException err)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Erro de formato, mensagem: {0}", err.Message);
+                }
+
+                Console.WriteLine("\n Pressione QUALQUER TECLA para finalizar o programa!");
+                Console.ReadKey();
+                Console.WriteLine("\n -- Fim do Programa -- \n");
+            */
+
+            // TESTE 2
+            Funcionario funcionario1 = new Funcionario();
+
+            try
+            {
+                funcionario1.AumentaSalario(-1000);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine();
+                System.Console.WriteLine("Houve um ArgumentException ao aumentar o salário!");
+            }
 
             Console.WriteLine("\n Pressione QUALQUER TECLA para finalizar o programa!");
             Console.ReadKey();
