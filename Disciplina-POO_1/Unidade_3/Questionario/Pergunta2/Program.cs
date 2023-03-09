5a18119e0c263c99adc135c0d47055d430b3bcce
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-08 22:17:35 -0300

    Pergunta 2

---
+++ Disciplina-POO_1/Unidade_3/Questionario/Pergunta2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Início namespace Pergunta2
+namespace Pergunta2
+{
+
+    // Inicio class Vegetal
+    class Vegetal
+    {
+        // Atributo
+        public string nome;
+    }
+    // Fimclass Vegetal
+
+    // Início class Program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vegetal tomate = new Vegetal();
+            Vegetal banana = tomate;
+
+            tomate.nome = "Carmem";
+            banana.nome = "Prata";
+
+            Console.WriteLine("\n {0}, {1}", tomate.nome, banana.nome);
+
+            Console.WriteLine("\n Pressione ENTER para finalizar o programa!");
+            Console.ReadKey();
+            Console.WriteLine("\n -- Fim do Programa -- \n");
+
+        }
+    }
+    // Fim class Program
+
+}
+// Fim namespace Pergunta2
