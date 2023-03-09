3f7046bd50a244f16dc395e6d8ccc27d1ccedd76
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-09 17:06:07 -0300

    Pergunta 1

---
+++ Disciplina-POO_1/Unidade_4/Questionario/Pergunta1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Início namespace Pergunta1
+namespace Pergunta1
+{
+
+    // Início class Program
+    class Program
+    {
+        // Início Main
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Olá, Mundo!");
+            Console.WriteLine("\n Pressione QUALQUER TECLA para finalizar o programa!");
+            Console.ReadKey();
+            Console.WriteLine("\n -- Fim do Programa -- \n");
+        }
+        // Fim Main
+    }
+    // Fim class Program
+
+}
+// Fim namespace Pergunta1
