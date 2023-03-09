5a4d3629cc1bd8407d65bc34b7ec0635fe2d3094
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-09 16:33:55 -0300

    Exemplo de tratamento de exceções erro de entrada.

---
+++ Disciplina-POO_1/Unidade_4/ExemploTRATAMENTOEXCECOES/Program.cs
@@ -0,0 +1,26 @@
+﻿// Início namespace ExemploTRATAMENTOEXCECOES
+namespace ExemploTRATAMENTOEXCECOES
+{
+
+    // Início class Program
+    class Program
+    {
+        // Início Main
+        static void Main(string[] args)
+        {
+            String v = "ABC";
+            double num = Convert.ToDouble(v); // Converte um string em número
+
+            Console.WriteLine();
+            Console.WriteLine(num);
+
+            Console.WriteLine("\n Pressione QUALQUER TECLA para finalizar o programa!");
+            Console.ReadKey();
+            Console.WriteLine("\n -- Fim do Programa -- \n");
+        }
+        // Fim Main
+    }
+    // Fim class Program
+
+}
+// Fim namespace ExemploTRATAMENTOEXCECOES
