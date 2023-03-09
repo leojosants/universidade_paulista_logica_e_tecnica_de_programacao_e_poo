d9ca9d56080222f445d70c916b717bb28a83857c
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-08 21:43:51 -0300

    Reutilização de classes - Herança

---
+++ Disciplina-POO_1/Unidade_3/Slides_Videoaula/ExemploHERANCA/Program.cs
@@ -0,0 +1,52 @@
+﻿// Início namespace ExemploHERANCA
+namespace ExemploHERANCA
+{
+
+    // Início class Pessoa
+    class Pessoa
+    {
+        // Atributo
+        public string nome;
+        public int telefone;
+
+        // Construtor
+        public Pessoa(string nome, int telefone)
+        {
+            this.nome = nome;
+            this.telefone = telefone;
+        }
+    }
+    // Fim class Pessoa
+
+    // Início class Aluno
+    class Aluno : Pessoa
+    {
+        // Atributo
+        public string registroAcademico;
+
+        // Contrutor
+        public Aluno(string nome, int telefone, string registroAcademico) : base(nome, telefone)
+        {
+            this.registroAcademico = registroAcademico;
+        }
+    }
+    // Fim class Aluno
+
+    // Início class Program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno1 = new Aluno("Leonardo", 32997711, "UNIP");
+            Console.WriteLine("\n Nome: {0} | Telefone: {1} | RA: {2}", aluno1.nome, aluno1.telefone, aluno1.registroAcademico);
+
+            Console.WriteLine("\n Pressione ENTER para finalizar o programa!");
+            Console.ReadKey();
+            Console.WriteLine("\n -- Fim do Programa -- \n");
+
+        }
+    }
+    // Fim class Program
+
+}
+// Fim namespace ExemploHERANCA
