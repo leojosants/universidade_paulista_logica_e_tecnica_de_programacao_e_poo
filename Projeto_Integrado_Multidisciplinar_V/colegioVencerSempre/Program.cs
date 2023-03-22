8eb5b32f54e16d23173b1b74270a88e5cbd0770a
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-22 12:43:35 -0300

    Criado classe Equipamento.

--- Projeto_Integrado_Multidisciplinar_V/colegioVencerSempre/Program.cs
+++ Projeto_Integrado_Multidisciplinar_V/colegioVencerSempre/Program.cs
@@ -39,10 +39,51 @@
 
         public override string ToString()
         {
-            return Nome + "" + idade + "" + sexo;
+            return " Nome:     " + Nome
+                + "\n Sexo:     " + Sexo
+                + "\n Idade:    " + Idade;
         }
 
     }   //  Fim class Pessoa
+
+    class Equipamento
+    {
+        private string nome = "";
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        private string codigo = "";
+        public string Codigo
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+
+        private bool livre;
+        public bool Livre
+        {
+            get { return livre; }
+            set { livre = value; }
+        }
+
+        public Equipamento()
+        {
+            this.nome = "";
+            this.codigo = "";
+            this.livre = true;
+        }
+
+        public Equipamento(string nome, string codigo)
+        {
+            this.nome = nome;
+            this.codigo = codigo;
+            this.livre = true;
+        }
+
+    }   //  Fim class Equipamento
 
 
     class Colaborador : Pessoa
@@ -63,6 +104,13 @@
             set { telefone = value; }
         }
 
+        private string senha = "";
+        public string Senha
+        {
+            get { return senha; }
+            set { senha = value; }
+        }
+
         private string tipo = "";
         public string Tipo
         {
@@ -79,19 +127,31 @@
 
         public Colaborador()
         {
-            this.email = "";
             this.telefone = "";
+            this.email = "";
+            this.senha = "";
             this.tipo = "";
             this.registro = numeroAleatorio.Next(100, 500);
         }
 
-        public Colaborador(string nome, string idade, string sexo, string email, string telefone, string tipo)
+        public Colaborador(string nome, string idade, string sexo, string telefone, string email, string senha, string tipo)
             : base(nome, idade, sexo)
         {
-            this.email = email;
             this.telefone = telefone;
+            this.email = email;
+            this.senha = senha;
             this.tipo = tipo;
             this.registro = numeroAleatorio.Next(100, 500);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString()
+                + "\n Tipo:     " + this.Tipo
+                + "\n Email:    " + this.Email
+                + "\n Senha:    " + this.Senha
+                + "\n Telefone: " + this.Telefone
+                + "\n Registro: " + this.Registro;
         }
 
     }   //  Fim class Colaborador
@@ -104,7 +164,34 @@
 
             Console.WriteLine("\nSISTEMA DE RESERVAS DE EQUIPAMENTOS - COLÉGIO VENCER");
 
+            Colaborador usuarioProfessor = new Colaborador();
+            usuarioProfessor.Idade = "38";
+            usuarioProfessor.Senha = "1234";
+            usuarioProfessor.Sexo = "Masculino";
+            usuarioProfessor.Tipo = "Professor";
+            usuarioProfessor.Telefone = "032999445588";
+            usuarioProfessor.Nome = "Usuário Professor";
+            usuarioProfessor.Email = "usuario@professor.com";
 
+            Console.WriteLine();
+            Console.WriteLine(usuarioProfessor);
+            Console.WriteLine();
+
+            Colaborador usuarioCoordenador = new Colaborador();
+            usuarioCoordenador.Idade = "58";
+            usuarioCoordenador.Senha = "4321";
+            usuarioCoordenador.Sexo = "Masculino";
+            usuarioCoordenador.Tipo = "Coordenaddor";
+            usuarioCoordenador.Telefone = "032944556412";
+            usuarioCoordenador.Nome = "Usuário Coordenador";
+            usuarioCoordenador.Email = "usuario@coordenador.com";
+
+            Console.WriteLine(usuarioCoordenador);
+            Console.WriteLine();
+
+            List<Colaborador> listaColaboradores = new List<Colaborador>();
+            listaColaboradores.Add(usuarioProfessor);
+            listaColaboradores.Add(usuarioCoordenador);
 
         }   //  Fim Main
 
