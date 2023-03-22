4c8932dfc04a7b4ab109a9a1ad85f0e9a9f6643c
Leonardo Santos <leonardojosantos@gmail.com>
2023-03-22 11:29:34 -0300

    Criado classe Pessoa.

--- Projeto_Integrado_Multidisciplinar_V/colegioVencerSempre/Program.cs
+++ Projeto_Integrado_Multidisciplinar_V/colegioVencerSempre/Program.cs
@@ -1,10 +1,52 @@
 ﻿namespace ColegioVencerSempre
 {
+    class Pessoa
+    {
+        private string nome = "";
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        private string sexo = "";
+        public string Sexo
+        {
+            get { return sexo; }
+            set { sexo = value; }
+        }
+
+        private int idade;
+        public int Idade
+        {
+            get { return idade; }
+            set { idade = value; }
+        }
+
+        public Pessoa()
+        {
+            this.nome = "";
+            this.idade = 0;
+            this.sexo = "";
+        }
+
+        public Pessoa(string nome, int idade, string sexo)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.sexo = sexo;
+        }
+
+    }   //  Fim class Pessoa
+
+
     class Program
     {
         static void Main(string[] args)
         {
-Console.WriteLine("Teste");
+
+            Console.WriteLine("Teste");
+
         }   //  Fim Main
 
     }   //  Fim class Program
